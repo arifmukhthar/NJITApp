36cdb951618638579b1449ce23f55271ea52b5cc
Arif <arif.mukhthar@gmail.com>
2015-09-23 17:46:45 -0400

    Layout
    change

---
+++ NJIT Student Guide/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  NJIT Student Guide
+//
+//  Created by Arif Mukhthar on 9/23/15.
+//  Copyright © 2015 Fantastic4. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
